ac93a6d923fe1e6b86c7cfe2aa2cb14725958d19
sharyu raut <sharyukraut210298@gmail.com>
2020-10-29 00:02:08 +0530

    [Sharyu] Add. Initial Commit

---
+++ IndianStateCensusAnalyser/CensusAdapter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace IndianStateCensusAnalyser
+{
+    class CensusAdapter
+    {
+        protected string[] GetCensusData(string csvFilePath, string dataHeader)
+        {
+            string[] censusData;
+            if (!File.Exists(csvFilePath))
+            {
+                throw new CensusAnalyserException("File Not Found", CensusAnalyserException.ExceptionType.FILE_NOT_FOUND);
+            }
+
+            if (Path.GetExtension(csvFilePath))
+            {
+                throw new CensusAnalyserException("Invalid file type", CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE);
+            }
+
+            censusData = File.ReadAllLines(csvFilePath);
+            if(censusData[0] != dataHeader)
+            {
+                throw new CensusAnalyserException("Incorrect header in Data", CensusAnalyserException.ExceptionType.INCORRECT_HEADER);
+            }
+            return censusData;
+        }
+    }
+}
